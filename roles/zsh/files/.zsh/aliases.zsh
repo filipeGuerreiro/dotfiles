daacf6fa3264aab57efd3df0114294fde1dadd18
filipe-guerreiro <filipe.guerreiro@linecorp.com>
2020-12-03 16:09:21 +0900

    Add color to ssh

--- roles/zsh/files/.zsh/aliases.zsh
+++ roles/zsh/files/.zsh/aliases.zsh
@@ -23,3 +23,5 @@ alias cp='cp -i'                              # confirm copy
 alias mv='mv -i'                              # confirm move
 alias cal='gcal --starting-day=1'             # print simple calendar for current month
 alias weather='curl v2.wttr.in'               # print weather for current location (https://github.com/chubin/wttr.in)
+
+alias ssh='TERM=xterm-256color \ssh'

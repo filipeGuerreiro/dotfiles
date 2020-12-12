2df93d213a61181dca7750bbde93876cf96b5a67
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2020-12-12 12:19:20 +0900

    Update emacs config

--- roles/emacs/files/doom.d/config.el
+++ roles/emacs/files/doom.d/config.el
@@ -10,22 +10,6 @@
 (setq user-full-name "Filipe Guerreiro"
       user-mail-address "filipe.m.guerreiro@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-solarized-light)
 
 ;; If you use `org' and don't want your org files in the default location below,
@@ -34,7 +18,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type nil)
+;;(setq display-line-numbers-type nil)
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
@@ -54,68 +38,34 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-;;(setq user-full-name "Filipe Guerreiro"
-;;             user-mail-address "filipe.m.guerreiro@gmail.com")
-;;(cond (IS-MAC
-;;            (setq mac-command-modifier      'meta
-;;                  mac-option-modifier       'alt
-;;                  mac-right-option-modifier 'super)))
-;;(setq confirm-kill-emacs nil)
-;;(setq auto-save-default t
-;;      make-backup-files t)
-;;(setq doom-font (font-spec :family "JetBrains Mono" :size 12)
-;;      doom-variable-pitch-font (font-spec :family "ETBembo" :size 12))
-;;(setq +doom-dashboard-banner-file
-;;      (expand-file-name "splash-images/black-hole2.png" doom-private-dir))
-;;(after! doom-modeline
-;;    (setq display-time-default-load-average nil)      ; don't show load average
-;;    (display-time-mode 1)                             ; Enable time in the mode-line
-;;    (display-battery-mode 1))                         ; On laptops it's nice to know how much power you have
-;;(setq evil-split-window-below t
-;;      evil-vsplit-window-right t)
-
-
-;;(setq org-directory "~/org/")
-;;(setq +pretty-code-enabled-modes '(org-mode))
-;;(after! org (setq org-hide-emphasis-markers t))
-;;(after! org (setq org-insert-heading-respect-content nil))
-;;(after! org
-;;      (setq org-log-done t)
-;;      (setq org-log-into-drawer t))
-;;(add-hook! org-mode :append
-;;           #'visual-line-mode
-;;           #'variable-pitch-mode
-;;           (lambda () (add-hook 'after-save-hook 'org-babel-tangle :append :local)))
-
-
-;;(after! company
-;;  (setq company-idle-delay 0.3
-;;        company-minimum-prefix-length 1
-;;  company-dabbrev-code-everywhere t
-;;  company-dabbrev-code-other-buffers 'all))
-;; company-quickhelp-delay 0.4)
-;;(after! prescient
-;;  (setq-default history-length 1000)
-;;  (setq-default prescient-history-length 1000))
-
-;; (add-hook 'after-init-hook 'company-statistics-mode))
-
-;; (set-company-backend! 'org-mode
-;;   '(company-capf company-files company-dabbrev-code))
-;;(after! company
-;;  (use-package company-tabnine :ensure t)
-;;  (setq company-backends '(company-tabnine company-capf company-yasnippet)))
-
-;; (after! company
-;;   (add-to-list 'company-backends 'company-tabnine))
-
 (setq doom-font (font-spec :family "JetBrains Mono" :size 13)
       doom-big-font (font-spec :family "JetBrains Mono" :size 16))
 (custom-set-faces!
   '(doom-modeline-buffer-modified :foreground "orange"))
 (global-set-key [f8] 'treemacs)
 
+(setq display-line-numbers-type 'relative)
 
+;; open as full-screen
+(if (eq initial-window-system 'x)                 ; if started by emacs command or desktop file
+    (toggle-frame-maximized)
+  (toggle-frame-fullscreen))
+
+;; window title
+(setq doom-fallback-buffer-name "► Doom"
+      +doom-dashboard-name "► Doom")
+(setq frame-title-format
+      '(""
+        (:eval
+         (if (s-contains-p org-roam-directory (or buffer-file-name ""))
+             (replace-regexp-in-string
+              ".*/[0-9]*-?" "☰ "
+              (subst-char-in-string ?_ ?  buffer-file-name))
+           "%b"))
+        (:eval
+         (let ((project-name (projectile-project-name)))
+           (unless (string= "-" project-name)
+             (format (if (buffer-modified-p)  " ◉ %s" "  ●  %s") project-name))))))
 
 ;;Other Projectile packages outside
 (projectile-add-known-project "~/writings")              ;My LaTeX Writings
@@ -126,3 +76,41 @@
 (setq projectile-project-search-path '("~/projects/" "~/work/"))
 (setq projectile-sort-order 'recentf)
 
+(setq which-key-idle-delay 0.5)
+
+(setq-default
+ delete-by-moving-to-trash t                      ; Delete files to trash
+ window-combination-resize t                      ; take new window space from all other windows (not just current)
+ x-stretch-cursor t)                              ; Stretch cursor to the glyph width
+
+(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
+      evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
+      auto-save-default t                         ; Nobody likes to loose work, I certainly don't
+      truncate-string-ellipsis "…")               ; Unicode ellispis are nicer than "...", and also save /precious/ space
+
+(display-time-mode 1)                             ; Enable time in the mode-line
+(unless (equal "Battery status not available"
+               (battery))
+  (display-battery-mode 1))                       ; On laptops it's nice to know how much power you have
+(global-subword-mode 1)                           ; Iterate through CamelCase words
+
+(after! lsp-python-ms
+  (set-lsp-priority! 'mspyls 1))
+
+;; preview buffers when splitting window
+(setq evil-vsplit-window-right t
+      evil-split-window-below t)
+(defadvice! prompt-for-buffer (&rest _)
+  :after '(evil-window-split evil-window-vsplit)
+  (+ivy/switch-buffer))
+(setq +ivy-buffer-preview t)
+
+;; completions all the time
+(after! company
+  (setq company-idle-delay 0.5
+        company-minimum-prefix-length 2)
+  (setq company-show-numbers t)
+  (add-hook 'evil-normal-state-entry-hook #'company-abort)) ;; make aborting less annoying.
+;; improve history memory
+(setq-default history-length 1000)
+(setq-default prescient-history-length 1000)
--- roles/emacs/files/doom.d/init.el
+++ roles/emacs/files/doom.d/init.el
@@ -24,13 +24,13 @@
        nav-flash                                ; blink cursor line after big motions
        ;;neotree                                ; a project drawer, like NERDTree for vim
        ophints                                  ; highlight the region an operation acts on
-       ;;(popup +defaults)                        ; tame sudden yet inevitable temporary windows
+       (popup +all +defaults)                   ; tame sudden yet inevitable temporary windows
        ;;tabs                                   ; a tab bar for Emacs
-       treemacs                                 ; a project drawer, like neotree but cooler
+       treemacs                               ; a project drawer, like neotree but cooler
        ;;unicode                                ; extended unicode support for various languages
-       ;;vc-gutter                                ; vcs diff in the fringe
+       vc-gutter                                ; vcs diff in the fringe
        vi-tilde-fringe                          ; fringe tildes to mark beyond EOB
-       window-select                          ; visually switch windows
+       (window-select +numbers)                 ; visually switch windows
        workspaces                               ; tab emulation, persistence & separate workspaces
        zen                                      ; distraction-free coding or writing
 
@@ -41,7 +41,7 @@
        (format +onsave)                       ; automated prettiness
        ;;god                                    ; run Emacs commands without modifier keys
        ;;lispy                                  ; vim for lisp, for people who don't like vim
-       ;;multiple-cursors                       ; editing in many places at once
+       multiple-cursors                       ; editing in many places at once
        ;;objed                                  ; text object editing for the innocent
        parinfer                               ; turn lisp into python, sort of
        ;;rotate-text                            ; cycle region at point between text candidates
@@ -49,10 +49,10 @@
        ;;word-wrap                              ; soft wrapping with language-aware indent
 
        :emacs
-       dired                                    ; making dired pretty [functional]
+       (dired +icons)                                    ; making dired pretty [functional]
        electric                                 ; smarter, keyword-based electric-indent
-       ;;ibuffer                                ; interactive buffer management
-       undo                                     ; persistent, smarter undo for your inevitable mistakes
+       (ibuffer +icons)                                ; interactive buffer management
+       (undo +tree)                                     ; persistent, smarter undo for your inevitable mistakes
        vc                                       ; version-control and Emacs, sitting in a tree
 
        :term
@@ -62,9 +62,9 @@
        vterm                                    ; the best terminal emulation in Emacs
 
        :checkers
-       (syntax +childframe)                     ; tasing you for every semicolon you forget
-       spell                                    ; tasing you for misspelling mispelling
-       ;;grammar                                ; tasing grammar mistake every you make
+       (syntax +childframe)                     ; teasing you for every semicolon you forget
+       spell                                    ; teasing you for misspelling mispelling
+       ;;grammar                                ; teasing grammar mistake every you make
 
        :tools
        ansible
@@ -75,17 +75,17 @@
        ;;ein                                    ; tame Jupyter notebooks with emacs
        (eval +overlay)                          ; run code, run (also, repls)
        gist                                     ; interacting with github gists
-       lookup                                   ; navigate your code and its documentation
+       (lookup +dictionary +docsets)            ; navigate your code and its documentation
        lsp
        (magit +forge)                           ; a git porcelain for Emacs
        make                                   ; run make tasks from Emacs
-       pass                                     ; password manager for nerds
+       ;;pass                                     ; password manager for nerds
        pdf                                      ; pdf enhancements
        ;;prodigy                                ; FIXME managing external services & code builders
-       ;;rgb                                    ; creating color strings
+       rgb                                    ; creating color strings
        taskrunner                             ; taskrunner for all your projects
-       ;;terraform                              ; infrastructure as code
-       tmux                                   ; an API for interacting with tmux
+       terraform                              ; infrastructure as code
+       ;;tmux                                   ; an API for interacting with tmux
        ;;upload                                 ; map local to remote projects via ssh/ftp
 
        :os
@@ -95,7 +95,7 @@
        :lang
        ;;agda                                   ; types of types of types of types...
        ;;cc                                     ; C/C++/Obj-C madness
-       clojure                                ; java with a lisp
+       (clojure +lsp)                           ; java with a lisp
        ;;common-lisp                            ; if you've seen one lisp, you've seen them all
        ;;coq                                    ; proofs-as-programs
        ;;crystal                                ; ruby at the speed of c
@@ -117,7 +117,7 @@
        ;;idris                                  ; a language you can depend on
        json                                     ; At least it ain't XML
        (java +meghanada)                      ; the poster child for carpal tunnel syndrome
-       javascript                             ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)                             ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia                                  ; a better, faster MATLAB
        kotlin                                 ; a better, slicker Java(Script)
        latex                                    ; writing papers in Emacs has never been so fun
@@ -134,14 +134,14 @@
        ;;php                                    ; perl's insecure younger brother
        plantuml                                 ; diagrams for confusing people more
        ;;purescript                             ; javascript, but functional
-       python                                   ; beautiful is better than ugly
+       (python +lsp)                                  ; beautiful is better than ugly
        ;;qt                                     ; the 'cutest' gui framework ever
        ;;racket                                   ; a DSL for DSLs
        ;;raku                                   ; the artist formerly known as perl6
        ;;rest                                   ; Emacs as a REST client
-       rst                                    ; ReST in peace
-       (ruby +rails)                          ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust                                   ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;rst                               :     ; ReST in peace
+       ;;(ruby +rails)                          ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (rust +lsp)                                   ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        scala                                  ; java, but good
        ;;scheme                                 ; a fully conniving family of lisps
        (sh +lsp)                                ; she sells {ba,z,fi}sh shells on the C xor
@@ -159,7 +159,7 @@
 
        :app
        ;;calendar
-       irc                                      ; how neckbeards socialize
+       ;;irc                                      ; how neckbeards socialize
        ;;(rss +org)                             ; emacs as an RSS reader
        ;;twitter                                ; twitter client https://twitter.com/vnought
 
